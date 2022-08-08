dfa26d51e03ec0704784a5efe2762f31f0d99683
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-08 11:54:20 +0200

    Initial Commit

---
+++ PreferencedKey/PreferencedKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferencedKeyApp.swift
+//  PreferencedKey
+//
+//  Created by Skorobogatow, Christian on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PreferencedKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
